7ef540035cf421d6743048003e123f303e6a9f63
Annish1234 <31614910+Annish1234@users.noreply.github.com>
2017-09-28 09:22:40 +0530

    Update test4.py

--- test4.py
+++ test4.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-import random 
+import random # it imports random
 count=0
 r=0
 while count<=200:
-	roll=input("press r to roll dice")
+	roll=input("press r to roll dice")# its the input give to run the programme
 	if roll=="r":
 		r=random.randint(1,6)
 		count=count+r
 		print("your random num is", r)
 		print("you are on count",count)																	
-	if count==8:
-		count=37
-		print("you got a ladder")
+	
 
 		
   
