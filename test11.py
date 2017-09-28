5725a95cb852f4865db52df746faaa389eac375e
Annish1234 <31614910+Annish1234@users.noreply.github.com>
2017-09-28 09:38:30 +0530

    Update test11.py

--- test11.py
+++ test11.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(1,10):
-	print(i*1,'\t',i*2,'\t',i*3,'\t',i*4,'\t',i*5,'\t')
+	print(i*1,'\t',i*2,'\t',i*3,'\t',i*4,'\t',i*5,'\t')# it multiplies the numbers and gives space / tab('/t')
