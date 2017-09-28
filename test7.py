c809c73bc4c0fba69590ce3a481a7135dd2947ce
Annish1234 <31614910+Annish1234@users.noreply.github.com>
2017-09-28 09:30:20 +0530

    Update test7.py

--- test7.py
+++ test7.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(5,50):		
-	if(i%5==0):					
+for i in range(5,50):	# you have given i in range 5 - 50 	
+	if(i%5==0):	# it divides the number from 5- 50 by 5				
 		print(i)	
 							
