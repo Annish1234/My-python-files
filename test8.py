91cfa63b50838a33359a18315c549c44612f9b02
Annish1234 <31614910+Annish1234@users.noreply.github.com>
2017-09-28 09:32:37 +0530

    Update test8.py

--- test8.py
+++ test8.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(2,20):		
-	if(i%2==0):					
-		print(i)	
+for i in range(2,20):	# you have given i in range 2-20	
+	if(i%2==0):	#it divides number from 2-20 by 2				
+		print(i)	#it does the same just you have to change the numbers
 for i in range(3,30):		
 	if(i%3==0):					
 		print(i)		
@@ -11,3 +11,4 @@ for i in range(4,40):
 for i in range(5,50):		
 	if(i%5==0):					
 		print(i)	
+		
