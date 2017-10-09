451e51baedb6d1c0ff526057e33cef5bf0a522d8
Annish1234 <31614910+Annish1234@users.noreply.github.com>
2017-10-09 10:42:35 +0000

    Add files via upload

---
+++ twitter2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import os 
+import time
+from gpiozero import MotionSensor
+from TwitterAPI import TwitterAPI
+CONSUMER_KEY = 'G5VEfOdfZC2BpVNN3VmIXhmp1'
+CONSUMER_SECRET = '2aUoYnMXSjhIGuBATwmamFnK7rb2CCs4eRFbYpcaVn33bhcosq'
+ACCESS_TOKEN_KEY = '910781772824068097-4UctxNpBE7ZCS0PcdKk18UwQ0oWyAhV'
+ACCESS_TOKEN_SECRET = 'TZcDnfeK6oFNsUOumAWWCHMAGPOQUatAWrxDMJZvdb6D9'
+api = TwitterAPI(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
+pir = MotionSensor(4)
+while True :
+    if  pir.motion_detected:
+            img="/home/pi/cam/imgs.jpg"
+            cmd="fswebcam -F 3 --fps 20 -r 800x600 " +img
+            os.system(cmd)
+            print ("pic taken")
+            file = open(img, 'rb')
+            data = file.read()
+            r = api.request('statuses/update_with_media', {'status':'pic from pyhton script'}, {'media[]':data})
+            print(r.status_code) 
+
+
+
+
+    
