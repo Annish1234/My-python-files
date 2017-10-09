6b1f9a7d7ed350e700ca42d87bd0a814a1094c77
Annish1234 <31614910+Annish1234@users.noreply.github.com>
2017-10-09 09:32:29 +0000

    Add files via upload

--- motion.py
+++ motion.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+import os 
+import time
 from gpiozero import MotionSensor
-
 pir = MotionSensor(4)
 while True:
     if pir.motion_detected:
         print("perimeter breached code red")
+        b=1
+        cmd="fswebcam -F 3 --fps 20 -r 800x600 cam"+str(b)+ ".jpg"
+        os.system(cmd)
+
