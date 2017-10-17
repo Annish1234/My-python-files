ff2cc7cd727e7df332d070bec07756b7603dc7eb
Annish1234 <31614910+Annish1234@users.noreply.github.com>
2017-10-17 12:55:43 +0530

    Add files via upload

---
+++ zodiac.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+print("")
+print("press 1 for aries. ")
+print("press 2 for capricorn")
+Userinput= int(input("whats your zodiac??"))
+if (Userinput == 1):
+					print (" you are good ")
+if (Userinput == 2):
+					print ("you are naive")		
+	 			
